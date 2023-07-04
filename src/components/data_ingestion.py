bcef74b65d9ce2a440aaf7e959a951383b70ac8c
Samar Chhetri <samarchhetri23@gmail.com>
2023-07-04 16:59:52 +0545

    data_ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,74 @@
+import os, sys
+from src.logger import logging
+from src.exception import CustomException
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import mysql.connector
+from mysql.connector import Error
+from src.utils import create_server_connection, create_db_connection, read_query
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifacts', 'train.csv')
+    test_data_path:str = os.path.join('artifacts', 'test.csv')
+    raw_data_path:str = os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion path")
+        try:
+            pw= 'Mysql#11'
+            db = 'crops'
+
+            q1 = """
+            select * from crop_details;
+            """
+
+
+            connection = create_server_connection(host_name= 'localhost', user_name='root', user_password=pw)
+            connection = create_db_connection(host_name='localhost', user_name='root', db_name=db, user_password=pw)
+
+            results = read_query(connection, q1)
+
+            from_db = []
+
+            for i in results:
+                res = list(i)
+                from_db.append(res)
+
+            df = pd.DataFrame(from_db, columns=['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall', 'label'])
+
+            logging.info("Read the dataset as dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info('Train test data initiated')
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=23)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info('Ingestion of data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
---
+++ src/utils.py
@@ -0,0 +1,74 @@
+import os, sys
+
+import pandas as pd
+import numpy as np
+import pickle
+import mysql.connector
+from mysql.connector import Error
+from sklearn.metrics import accuracy_score
+
+from src.exception import CustomException
+
+
+# To create server connection
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+        host = host_name,
+        user = user_name,
+        passwd = user_password
+        )
+        print("MySQL server connection successful")
+    
+    
+    except Error as e:
+        print(f" Error: '{e}'")
+        
+    return connection
+
+
+
+
+
+# To connect to database
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection =None
+    try:
+        connection = mysql.connector.connect(
+        host = host_name,
+        user = user_name,
+        passwd = user_password,
+        database = db_name
+        )
+        print("MySQL databse connection successfully")
+    
+    except Error as e:
+        print(f"Error : '{e}'")
+    return connection
+
+
+
+# To execute query
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query was successful")
+    
+    
+    except Error as e:
+        print(f"Error : '{e}'")
+
+
+# To read query
+def read_query(connection, query):
+    cursor =connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"Error : '{e}'")
